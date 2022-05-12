cc7c2a8aca7eba54219af77fc6108ad3149a59de
alex919090 <alex919090@gmail.com>
2022-05-11 20:47:37 -0400

    creacion de documentjs

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,4 @@
-import '../styles/globals.css';
+import '@styles/globals.css';
 import Header from '@components/Header';
 import AppContext from '@context/AppContext';
 import useInitialState from '@hooks/useInitialState';
